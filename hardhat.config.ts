b5249756c9e7b38a574333d0ae7c48c2f399fa99
MyTwixer <ihorbudyak@gmail.com>
2024-02-22 22:02:06 +0100

    update

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,10 +1,26 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-foundry";
+// import "@nomiclabs/hardhat-etherscan";
+import dotenv from 'dotenv'; 
+dotenv.config();  
+
+const privateKey = "0x" + process.env.API_URL;
 
 
 const config: HardhatUserConfig = {
+  defaultNetwork: "sepolia",
   solidity: "0.8.24",
+  networks: {
+    sepolia: {
+      url : process.env.URI,
+      accounts: [privateKey],
+
+    }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_KEY
+  }
 };
 
 export default config;
