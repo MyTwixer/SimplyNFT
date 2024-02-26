fe1acefdd21b2c0f52980d39c86dc6e3035cff1a
MyTwixer <ihorbudyak@gmail.com>
2024-02-26 10:38:00 +0100

    update

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -2,9 +2,10 @@ import { ethers } from "hardhat";
 
 async function main() {
 
-  const simplyNFT = await ethers.deployContract("SimplyToken");
+  const simplyNFT = await ethers.deployContract("SimplyToken", ['0xbffa4029D29F5b0A659196972082dfe17e569937']);
 
   await simplyNFT.waitForDeployment();
+  console.log(`Address: ${simplyNFT.target}`)
   
 }
 main().catch((error) => {
